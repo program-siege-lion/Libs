26e6cb768ae5d912140a8f715099c1b1d297c31f
program-siege-lion <sun_shao_lei@163.com>
2020-07-10 20:52:13 +0800

    first commit

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+require "vendor/autoload.php";
+
+$obj = new sunxiaolei\Test();
+$obj->test();
---
+++ src/sunxiaolei/Test.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace sunxiaolei;
+
+class Test {
+    public function test(){
+        echo 111;
+    }
+}
